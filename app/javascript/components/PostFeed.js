5b2edf8bddc216861edbea44044020494c754057
Siona Vivian <SionaSVivian@outlook.com>
2021-02-22 17:34:26 -0800

    reorganize stylesheets add arrow icon create postfeed component modify styles

--- app/javascript/components/PostFeed.js
+++ app/javascript/components/PostFeed.js
@@ -4,10 +4,23 @@ const PostFeed = (props) => {
   return (
     <React.Fragment>
       {props.posts.map((post) => (
-        <li>
-          <h1>{post.title}</h1>
-          <div>{post.score}</div>
-          <p>{post.description}</p>
+        <li class="postfeed-post-container">
+          <div className="vertical-score-post-container">
+            <button className="arrow arrow-up"></button>
+            <div class="postfeed-score">
+              {(() =>
+                post.score > 1000
+                  ? (post.score / 1000).toString().slice(0, 3) + "K"
+                  : post.score)()}
+            </div>
+            <button className="arrow arrow-down"></button>
+          </div>
+          <div className="post-details-container">
+            <div className="post-metadata"></div>
+            <h1>{post.title}</h1>
+            <p>{post.description}</p>
+          </div>
+          <div className="post-image"></div>
         </li>
       ))}
     </React.Fragment>
