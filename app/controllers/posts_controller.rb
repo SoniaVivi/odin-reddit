375c08683524f86824264324a3006d2288938f0b
Siona Vivian <SionaSVivian@outlook.com>
2021-02-26 16:26:58 -0800

    modify user validations seed file Post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = (Post.select(:title, :description, :score, :subject_id,
+                          :created_at, :updated_at, :origin_id)
+                 .order(created_at: :desc))
   end
   def show
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :name, presence: true
+  validates :name, length: { in: 3..24}
 
   has_many :posts, foreign_key: 'poster_id'
 end
